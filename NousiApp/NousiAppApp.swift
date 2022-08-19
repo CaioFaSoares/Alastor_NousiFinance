7d78a09362987c16928adc8dd29d7adc2e5ef490
Caio <caiofasoares@gmail.com>
2022-08-19 11:32:04 -0300

    Initial Commit

---
+++ NousiApp/NousiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NousiAppApp.swift
+//  NousiApp
+//
+//  Created by Caio Soares on 19/08/22.
+//
+
+import SwiftUI
+
+@main
+struct NousiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
