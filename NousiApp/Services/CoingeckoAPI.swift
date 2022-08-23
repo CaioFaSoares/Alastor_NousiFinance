8e8d2baee945f6ed7dfbba7e6ff6b40e49ad293f
Caio Soares <caiofasoares@gmail.com>
2022-08-23 11:15:02 -0300

    refactoring teamchoicemodel to fit with the coingecko api answer

--- NousiApp/Services/CoingeckoAPI.swift
+++ NousiApp/Services/CoingeckoAPI.swift
@@ -8,7 +8,9 @@
 import Foundation
 
 struct GeckoApi {
+    
     static let baseURL: String = "api.coingecko.com/api/v3"
+    
     static func getAllCoinsMarketData(currency: String = "usd", ids: String, category: String, order: String = "market_cap_desc", sparkline: Bool = false) async {
         var url = URLComponents(string: GeckoApi.baseURL)!
         
