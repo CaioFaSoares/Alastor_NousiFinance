bb9f9fedc0fe9720a45589c6a38238021284ce0c
otavioalbucosta <otavio-albucosta@hotmail.com>
2022-08-23 10:42:39 -0300

    Coin mode done, getCoinsMarket service started

--- NousiApp/Models/CoinModel.swift
+++ NousiApp/Models/CoinModel.swift
@@ -8,3 +8,24 @@
 import Foundation
 
 
+struct Coin: Codable {
+    
+    var id: String
+    var symbol: String
+    var name: String
+    var price: Double
+    var priceChangePercentage: Double
+    var high24h: Double
+    var low24h: Double
+    
+    
+    enum CodingKeys: String, CodingKey {
+        case id, symbol, name
+        case price = "current_price"
+        case priceChangePercentage = "price_change_percentage_24h"
+        case high24h = "high_24h"
+        case low24h = "low_24h"
+        
+    }
+    
+}
--- NousiApp/Services/CoingeckoAPI.swift
+++ NousiApp/Services/CoingeckoAPI.swift
@@ -7,3 +7,18 @@
 
 import Foundation
 
+struct GeckoApi {
+    static let baseURL: String = "api.coingecko.com/api/v3"
+    static func getAllCoinsMarketData(currency: String = "usd", ids: String, category: String, order: String = "market_cap_desc", sparkline: Bool = false) async {
+        var url = URLComponents(string: GeckoApi.baseURL)!
+        
+        url.queryItems = [
+            URLQueryItem(name: "vs_currency", value: currency),
+            URLQueryItem(name: "ids", value: ids)
+        ]
+        var request = URLRequest(url: url.url!)
+        
+    }
+    
+    
+}
