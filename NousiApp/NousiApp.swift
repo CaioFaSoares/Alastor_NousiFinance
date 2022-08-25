a7781579787a9905c985b8850b956ec265605c28
Lucas Barbosa de Oliveira <lucasbarbosa2807@gmail.com>
2022-08-25 20:33:20 -0300

    Feat: including timer to splash

--- NousiApp/NousiApp.swift
+++ NousiApp/NousiApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct NousiApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LaunchScreen()
                 }
         }
 }
--- NousiApp/Views/SplashScreen.swift
+++ NousiApp/Views/SplashScreen.swift
@@ -2,36 +2,51 @@ import Foundation
 import SwiftUI
 
 struct LaunchScreen: View {
+    
+    @State private var isActive = false
+    
     var body: some View {
-        ZStack{
-            Color.green
-            
-            VStack{
+        if isActive{
+            ContentView()
+        } else {
+            ZStack{
+                Color.green
                 
+                VStack{
+                    
+                    
+                    Spacer()
+                    
+                    Image("NousiLogo")
+                        .resizable()
+                        .frame(width: 200, height: 200)
+                    
+                    ProgressView(value: 0.75)
+                        .progressViewStyle(CircularProgressViewStyle())
+                    Spacer()
+                    
+                    HStack{
+                        Text("Developed by")
+                        Text("Copland").foregroundColor(Color.blue)
+                    }.font(.system(size: 10))
+                        .padding(.bottom, 20)
+                    
+                }
+            }.ignoresSafeArea()
+                .onAppear(){
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 2.0){
+                        self.isActive = true
+                    }
+                }
                 
-                Spacer()
-                
-                Image("NousiLogo")
-                    .resizable()
-                    .frame(width: 200, height: 200)
-                
-                ProgressView(value: 0.75)
-                    .progressViewStyle(CircularProgressViewStyle())
-                Spacer()
-                
-                HStack{
-                    Text("Developed by")
-                    Text("Copland").foregroundColor(Color.blue)
-                }.font(.system(size: 10))
-                    .padding(.bottom, 20)
-                
-            }
-            
-        }.ignoresSafeArea()
+        }
+        
         
             
     }
 }
+
+
 
 struct LaunchScreen_Previews: PreviewProvider {
     static var previews: some View {
